f8f89fdd43f1c9f3cbce106a9655c55811e134b1
DongYu Tan <dytan@pop-os.localdomain>
2022-10-20 15:00:56 +0800

    break main into multiple single functions

--- lab/lab02/lab02question1.cpp
+++ lab/lab02/lab02question1.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
+struct employee {
+  std::string name;
+  float salary;
+};
 
-  struct employee {
-    std::string name;
-    float salary;
-  };
+int getNumberOfEmployee() {
 
   int numberOfEmployee;
 
   std::cout << "Enter number of employess: ";
   std::cin >> numberOfEmployee;
 
-  employee *employees = new employee[numberOfEmployee];
+  return numberOfEmployee;
+};
 
+employee *getArray(int numberOfEmployee) {
+  return new employee[numberOfEmployee];
+};
+
+void readEmployees(employee *employees, int numberOfEmployee) {
   for (int i = 0; i < numberOfEmployee; i++) {
     std::cout << "Employee " << i + 1 << std::endl;
     std::cout << "\t Enter name: ";
@@ -23,15 +29,32 @@ int main(int argc, char *argv[]) {
     std::cin >> employees[i].salary;
     std::cout << std::endl;
   }
+};
 
+void header() {
   std::cout << "*****Employee Details*****" << std::endl;
   std::cout << std::endl;
+};
 
+void printEmployee(employee *employees, int numberOfEmployee) {
   for (int i = 0; i < numberOfEmployee; i++) {
     std::cout << "Name: " << employees[i].name << std::endl;
     std::cout << "Salary: " << employees[i].salary << std::endl;
     std::cout << std::endl;
   }
+};
+
+int main(int argc, char *argv[]) {
+
+  int numberOfEmployee = getNumberOfEmployee();
+
+  employee *employees = getArray(numberOfEmployee);
+
+  readEmployees(employees, numberOfEmployee);
+
+  header();
+
+  printEmployee(employees, numberOfEmployee);
 
   return 0;
 }
